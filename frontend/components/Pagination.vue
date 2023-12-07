39c8da7961276c373e049243415000a980dea5e6
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-12-07 12:00:57 +0700

    make pagination component

--- frontend/components/Pagination.vue
+++ frontend/components/Pagination.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="flex items-center space-x-2">
     <button
-      class="bg-transparent border-transparent w-auto h-auto text-gray-400 disabled:text-gray-300 hover:text-primary cursor-pointer transition"
+      class="bg-transparent border-transparent w-auto h-auto text-gray-500 disabled:text-gray-300 hover:text-primary cursor-pointer transition"
       @click="$emit('update:modelValue', 1)">
       <Icon class="w-3 h-3" type="chevron-double-left"></Icon>
     </button>
     <button
-      class="bg-transparent border-transparent w-auto h-auto text-gray-400 disabled:text-gray-300 hover:text-primary cursor-pointer transition"
+      class="bg-transparent border-transparent w-auto h-auto text-gray-500 disabled:text-gray-300 hover:text-primary cursor-pointer transition"
       @click="$emit('update:modelValue', modelValue - 1)" :disabled="modelValue <= 1">
       <Icon class="w-3 h-3" type="chevron-left"></Icon>
     </button>
-    <div class="text-gray-700">
+    <div class="text-gray-600">
       {{ modelValue }} - {{ totalPage }}
     </div>
     <button
-      class="bg-transparent border-transparent w-auto h-auto text-gray-400 disabled:text-gray-300 hover:text-primary cursor-pointer transition"
+      class="bg-transparent border-transparent w-auto h-auto text-gray-500 disabled:text-gray-300 hover:text-primary cursor-pointer transition"
       @click="$emit('update:modelValue', modelValue + 1)" :disabled="modelValue >= totalPage">
       <Icon class="w-3 h-3" type="chevron-right"></Icon>
     </button>
     <button
-      class="bg-transparent border-transparent w-auto h-auto text-gray-400 disabled:text-gray-300 hover:text-primary cursor-pointer transition"
+      class="bg-transparent border-transparent w-auto h-auto text-gray-500 disabled:text-gray-300 hover:text-primary cursor-pointer transition"
       @click="$emit('update:modelValue', totalPage)">
       <Icon class="w-3 h-3" type="chevron-double-right"></Icon>
     </button>
